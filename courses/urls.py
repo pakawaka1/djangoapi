263ca04c00a7120608835f2c292081c30c893a88
pakawaka1 <davidcervantesdev@gmail.com>
2020-02-22 16:34:39 -0800

    first real commit

---
+++ courses/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url, include
+from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('courses', views.CourseView)
+
+urlpatterns = [
+    url('', include(router.urls)),
+]
